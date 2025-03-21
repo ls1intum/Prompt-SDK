f7c175cb476fd7fba5cc8ab5b5e94deed089d568
Stefan Niclas Heun <niclas.heun@tum.de>
2025-03-21 15:56:21 +0100

    exporting the middleware

--- auth.go
+++ auth.go
@@ -1,9 +1,14 @@
 package promptSDK
 
 import (
+	"github.com/gin-gonic/gin"
 	"github.com/ls1intum/prompt-sdk/keycloakTokenVerifier"
 )
 
 func InitAuthenticationMiddleware(KeycloakURL, Realm, CoreURL string) error {
 	return keycloakTokenVerifier.InitKeycloakTokenVerifier(KeycloakURL, Realm, CoreURL)
+}
+
+func AuthenticationMiddleware(allowedRoles ...string) gin.HandlerFunc {
+	return keycloakTokenVerifier.AuthenticationMiddleware(allowedRoles...)
 }
