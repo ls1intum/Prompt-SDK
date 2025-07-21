a9597fb8e6fa211e0b063420bb9c0bcb2c2a7356
Maximilian Rapp <maximilian.rapp@tum.de>
2025-07-21 08:57:08 +0200

    Enhance Team struct: Add binding tags for validation on ID, Name, Members, and Tutors fields

--- promptTypes/team.go
+++ promptTypes/team.go
@@ -3,8 +3,8 @@ package promptTypes
 import "github.com/google/uuid"
 
 type Team struct {
-	ID      uuid.UUID `json:"id"`
-	Name    string    `json:"name"`
-	Members []Person  `json:"members"`
-	Tutors  []Person  `json:"tutors"`
+	ID      uuid.UUID `json:"id" binding:"uuid"`
+	Name    string    `json:"name" binding:"required"`
+	Members []Person  `json:"members" binding:"dive"`
+	Tutors  []Person  `json:"tutors" binding:"dive"`
 }
