2f4be082de45dfd0d63aab68ea1e2f9b39d807d0
Mathilde Hagl <mathilde.s.hagl@tum.de>
2025-12-16 16:44:30 +0100

    Add initKeycloak function for Keycloak initialization

---
+++ initKeycloak.go
@@ -0,0 +1,24 @@
+package promptSDK
+
+import (
+	"strings"
+
+	"github.com/ls1intum/prompt-sdk/utils"
+	log "github.com/sirupsen/logrus"
+)
+
+func initKeycloak() {
+	baseURL := GetEnv("KEYCLOAK_HOST", "http://localhost:8081")
+	if !strings.HasPrefix(baseURL, "http") {
+		log.Warn("Keycloak host does not start with http(s). Adding https:// as prefix.")
+		baseURL = "https://" + baseURL
+	}
+
+	realm := GetEnv("KEYCLOAK_REALM_NAME", "prompt")
+
+	coreURL := utils.GetCoreUrl()
+	err := InitAuthenticationMiddleware(baseURL, realm, coreURL)
+	if err != nil {
+		log.Fatalf("Failed to initialize keycloak: %v", err)
+	}
+}
